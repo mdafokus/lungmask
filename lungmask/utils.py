2ffebb1340ca3dcd08a29aac2f49728adbba66a2
mda_fokus <darya.martyniuk@fokus.fraunhofe.de>
2022-10-26 14:53:58 +0200

    remove fill_voids dependency

--- lungmask/utils.py
+++ lungmask/utils.py
@@ -7,7 +7,6 @@ import sys
 import SimpleITK as sitk
 import pydicom as pyd
 import logging
-import fill_voids
 import skimage.morphology
 
 
@@ -239,8 +238,8 @@ def postrocessing(label_image, spare=[]):
     if outmask_mapped.shape[0] == 1:
         # holefiller = lambda x: ndimage.morphology.binary_fill_holes(x[0])[None, :, :] # This is bad for slices that show the liver
         holefiller = lambda x: skimage.morphology.area_closing(x[0].astype(int), area_threshold=64)[None, :, :] == 1
-    else:
-        holefiller = fill_voids.fill
+    #else:
+    #    holefiller = fill_voids.fill
 
     outmask = np.zeros(outmask_mapped.shape, dtype=np.uint8)
     for i in np.unique(outmask_mapped)[1:]:
